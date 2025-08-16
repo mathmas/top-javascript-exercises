3839d9f42ce714df18e171a55c153304d91a81c8
SakolBien <sakolbien@desk.home>
2025-08-16 18:46:01 +0200

    feat: exercise 07 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split("");
+    let returnString = "";
+    for(let i = arr.length -1; i >= 0; i--){
+        returnString += arr[i];
+    }
+    return returnString;
 };
 
 // Do not edit below this line
