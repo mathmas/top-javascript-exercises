ddb252ec9748d88b49418c2c28b537805e5f2954
SakolBien <sakolbien@desk.home>
2025-08-16 19:20:46 +0200

    feat: exercise 09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0) return "ERROR";
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+    let sum = 0;
+    let bigger;
+    let smaller;
+    if(a < b){
+        bigger = b;
+        smaller = a;
+    }else{
+        bigger = a;
+        smaller = b;
+    }
+
+
+    for(let i = smaller; i <= bigger; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
