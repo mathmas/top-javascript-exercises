89fea6b1d6aeb35601b2e5d3b307b36bcd844a68
SakolBien <sakolbien@desk.home>
2025-08-16 19:05:02 +0200

    feat: exercise 08 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    for (const value of values) {
+        while (arr.findIndex((arrVal) => arrVal === value) >= 0) {
+            arr.splice(arr.findIndex((arrVal) => arrVal === value), 1);
+        }
+    }
+
+    console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
