235f712413e9d91577204d096f9fad2d115534bf
Mathieu <mathieu221605@yahoo.fr>
2025-08-18 08:53:58 +0200

    feat: exercise 11 done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+// Formula => (xF - 32) * 5/9 = yC
+
+const convertToCelsius = function(fahrenheit) {
+  const temp = (fahrenheit - 32) * 5/9;
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// Formula => (xC * 9/5) + 32 = yF
+const convertToFahrenheit = function(celsius) {
+  const temp = (celsius * 9/5) + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
