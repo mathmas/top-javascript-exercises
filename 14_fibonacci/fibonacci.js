8eae0cc0a4b1fca1f5a89fe6ed4cf8ee35a4486a
Mathieu <mathieu221605@yahoo.fr>
2025-09-11 14:11:23 +0200

    feat: exercice 14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    //Check input
+    //Convert strings to int
+    int = parseInt(int);
+    //Don't accept negatives
+    if(int < 0){ 
+        return "OOPS";
+    }
 
+    if(int == 0) return 0;
+    if(int == 1) return 1;
+    if(int == 2) return 1;
+
+    return fibonacci(int - 2) + fibonacci(int - 1);
 };
 
 // Do not edit below this line
