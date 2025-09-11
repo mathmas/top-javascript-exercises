dfd18e668bf67dac6d702479a11ba4549b0f5987
Mathieu <mathieu221605@yahoo.fr>
2025-09-11 13:48:01 +0200

    feat: exercice 13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const availablesChars = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+const palindromes = function (string) {
+    //format string
+    const formatedSting = string.toLowerCase()
+    .split("")
+    .filter((character) => availablesChars.includes(character))
+    .join("");
+
+    //create a reverse string
+    const reversedString = formatedSting.split("")
+    .reverse()
+    .join("");
+
+    //compare strings
+    return formatedSting == reversedString;
 };
 
 // Do not edit below this line
