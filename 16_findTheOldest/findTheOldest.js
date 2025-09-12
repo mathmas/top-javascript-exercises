936ed63a420c28397abb5cb1e87f73258d8ee545
Mathieu <mathieu221605@yahoo.fr>
2025-09-12 14:21:18 +0200

    feat: exercice 16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest)
+        const currentAge = getAge(current);
 
+        return oldestAge > currentAge ? oldest : current;
+    })
+
+    function getAge(person){
+        if(!person.yearOfDeath){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+
+        return person.yearOfDeath - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
